ae6f4099ff364bb7858c98b923501432394783f3
rahul-nakum14 <106817606+rahul-nakum14@users.noreply.github.com>
2022-10-22 23:49:37 +0530

    Update hostnameresolver.py

--- hostnameresolver.py
+++ hostnameresolver.py
@@ -1,14 +1,36 @@
+import threading
 import requests
 import bs4
+import time
 
-domain = "https://"+input()
-request_url = requests.get(domain)
-domaintext = request_url.text 
+start = time.perf_counter()
+def test ():
+    with open ('c:\\Users\\rahul\\Desktop\\Python\\codechef\\Python Project\\domain.txt','r') as f:
 
-soup = bs4.BeautifulSoup(domaintext,'html.parser')
-print(request_url.status_code)
-print(soup.find("title").text)
+    
+        start = time.perf_counter()
+        for i in f:
+            tttt = (i.strip())
+            domain = "https://"+tttt
+        
+            request_url = requests.get(domain)
+            domaintext = request_url.text 
 
+            soup = bs4.BeautifulSoup(domaintext,'html.parser')
+            print(request_url.status_code)
+            print(soup.find("title").text)
+            time.sleep(0.01)
+
+        finissh = time.perf_counter()
+        print(f'Finished in {round(finissh-start ,2)} second (s)')
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=test)
+    t1.start()
+
+
+    
+# print(start - 2789200.5593500,2)
 
 '''
 
