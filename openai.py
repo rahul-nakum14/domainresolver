420b4e222d227a90996c91a80f7caae8affdeddb
rahul-nakum14 <106817606+rahul-nakum14@users.noreply.github.com>
2022-12-04 00:59:03 +0530

    Update openai.py

--- openai.py
+++ openai.py
@@ -1,3 +1,34 @@
+def test():
+    start = time.perf_counter()
+    try:
+        with open(sys.argv[1], 'r') as f:
+            for i in f:
+                tmp = site_is_online((i.strip()))
+                if tmp:
+                    domain = "http://" + (i.strip())
+                    try:
+                        request_url = requests.get(
+                            domain, timeout=2, allow_redirects=True)
+                        domaintext = request_url.text
+                        soup = bs4.BeautifulSoup(domaintext, 'html.parser')
+                        code1 = request_url.status_code
+                        title1 = soup.find("title").text
+                        output_line = f"{CYAN}{request_url.url}{NC}{RED}{[code1]}{NC} {BLUE}{[title1]}{NC}"
+                        if args.output:
+                            with open(args.output, 'w') as f:
+                                f.write(output_line + '\n')
+                        else:
+                            print(output_line)
+                    except Exception:
+                        print(f"{LRED}{domain} [Something Went Wrong] {NC}")
+                else:
+                    print(f"{LRED}{i.strip()} [Domain is not found] {NC}")
+        finissh = time.perf_counter()
+        print(f'Finished in {round(finissh-start ,2)} second (s)')
+    except Exception as e:
+        print("Provide domain file")
+
+
 # import sys
 # import argparse
 # import requests
@@ -19,13 +50,13 @@
 #                             Output.writelines(i.strip())
 
 
-import argparse
+# import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("domain.txt",help="input domain file")
-parser.add_argument("-o","--output",help="Write output to file")
-args = parser.parse_args()
+# parser = argparse.ArgumentParser()
+# parser.add_argument("domain.txt",help="input domain file")
+# parser.add_argument("-o","--output",help="Write output to file")
+# args = parser.parse_args()
 
-# Use the args.output variable to specify the output file name
-with open(args.output, 'w') as output_file:
-    output_file.write("xssxsxsx")
+# # Use the args.output variable to specify the output file name
+# with open(args.output, 'w') as output_file:
+#     output_file.write("xssxsxsx")
