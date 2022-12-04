0ded4366e6f17495241cd63b07f2082aee3fc166
rahul-nakum14 <106817606+rahul-nakum14@users.noreply.github.com>
2022-12-04 13:41:23 +0530

    Update final_sys.py

--- final_sys.py
+++ final_sys.py
@@ -2,104 +2,65 @@ import requests
 import bs4
 import time
 import sys
+import argparse
+import os
 
 RED='\033[0;31m'
 NC='\033[0m'
-BLUE = '\033[0;34m'
-CYAN = '\033[0;36m'
-LRED = '\033[1;31m'
+BLUE = '\033[0;94m'
+CYAN = '\033[0;96m'
+LRED = '\033[1;91m'
+Green= '\033[0;92m'  
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-i","--input",help="input domain file")
+parser.add_argument("-o","--output",help="Write output to file")
+args = parser.parse_args()
+outputdata = []
 
-def test ():
-        start = time.perf_counter()
-        try:
-                with open (sys.argv[1],'r') as f:
-                        for i in f:
-                                tmp = site_is_online((i.strip()))
-                                if tmp:
-                                        # if 'http://' in tmp:
-                                        domain = "http://"+(i.strip())
-                                        # if 'https://' in tmp:
-                                        #          domain = "https://"+(i.strip())
-                                        try:
-                                                                # domain = "https://"+(i.strip())
-                                                                request_url = requests.get(domain,timeout=2,allow_redirects=True)
-                                                                domaintext = request_url.text 
-                                                                soup = bs4.BeautifulSoup(domaintext,'html.parser')
-                                                                code1 = request_url.status_code
-                                                                title1 = soup.find("title").text
-                                                                print(f"{CYAN}{request_url.url}{NC}{RED}{[code1]}{NC} {BLUE}{[title1]}{NC}")
-                                        except Exception:
-                                                                print(f"{LRED}{domain} [Something Went Wrong] {NC}")
+def main():
+    start = time.perf_counter()
+    try:
+        with open(sys.argv[2], 'r') as f:
+            for i in f:
+                tmp = site_is_online((i.strip()))
+                if tmp:
+                    domain = "http://" + (i.strip())
+                    try:
+                        request_url = requests.get(
+                            domain, timeout=2, allow_redirects=True)
+                        domaintext = request_url.text
+                        soup = bs4.BeautifulSoup(domaintext, 'html.parser')
+                        code1 = request_url.status_code
+                        title1 = soup.find("title").text
+                        print(f"{CYAN}{request_url.url}{NC}{RED}{[code1]}{NC} {BLUE}{[title1]}{NC}")
+                        outputdata.append(f"{CYAN}{request_url.url}{NC}{RED}{[code1]}{NC} {BLUE}{[title1]}{NC}")
+                        
+                    except Exception:
+                        print(f"{Green}{domain} [Please take a look Manually.] {NC}")
+                else:
+                        print(f"{LRED}{i.strip()} [Domain is not found] {NC}")
+                        outputdata.append(f"{LRED}{i.strip()} [Domain is not found] {NC}")
 
-                                else:
-                                        print(f"{LRED}{i.strip()} [Domain is not found] {NC}")
-                finissh = time.perf_counter()
-                print(f'Finished in {round(finissh-start ,2)} second (s)')
-        except Exception as e:
-                print("Provide domain file")
+        if args.output:
+                                with open(args.output, 'w') as f:
+                                        for i in outputdata:
+                                                f.write(i + '\n')
+
+    except Exception as e:
+        print("Provide domain file")
+
+        finissh = time.perf_counter()
+        print(f'Finished in {round(finissh-start ,2)} second (s)')
 
 
 def site_is_online(url,timeout=2):
-        tmp = 'http://'+url
-        try:
-
-                if requests.head('http://'+url,timeout=timeout):
-                        return tmp
-        except Exception :
-                return
-        #     print(f"{LRED}{url} [Domain is not found] {NC}")
-            
+    try:
+        if requests.head('http://'+url,timeout=timeout):
+            return 'http://'+url
+    except Exception:
+        return None
 
 
 if __name__ == "__main__":
-        test()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def site_checker(url):
-#     connection = HTTPConnection(host=url,timeout=2)
-#     try:
-#         connection.request('HEAD',"/")
-#         return True
-#     except Exception:
-#         print(f"{LRED}{url} [Domain is not exist]{NC}")
-        
-        
-
-                
-
-    # avail()
-    # t1 = threading.Thread(target=test)
-    # t1.start()
-
-
-
+        main()
